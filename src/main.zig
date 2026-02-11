e7a155ad07fa99a4900225b273ebad1ca0b43c50
arod1213 <aidan@aidanrodriguez.com>
2026-02-11 14:51:45 -0800

    menu bar app setup

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,26 @@
 const std = @import("std");
 const launcher = @import("launcher");
-const posix = std.posix;
+const menuczar = @import("menuczar");
 
-fn setupTermios(handle: posix.fd_t) !void {
-    var settings = try posix.tcgetattr(handle);
-    settings.lflag.ICANON = false;
-    settings.lflag.ECHO = false;
-    _ = try posix.tcsetattr(handle, posix.TCSA.NOW, settings);
-}
+// const posix = std.posix;
+// fn setupTermios(handle: posix.fd_t) !void {
+//     var settings = try posix.tcgetattr(handle);
+//     settings.lflag.ICANON = false;
+//     settings.lflag.ECHO = false;
+//     _ = try posix.tcsetattr(handle, posix.TCSA.NOW, settings);
+// }
 
-pub fn main() !void {
-    // const stdin = std.fs.File.stdin();
-    // try setupTermios(stdin.handle);
+// const stdin = std.fs.File.stdin();
+// try setupTermios(stdin.handle);
 
+fn appLoop() void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const alloc = arena.allocator();
 
-    try launcher.run(alloc);
+    launcher.run(alloc) catch @panic("failed to run app");
+}
+
+pub fn main() !void {
+    try menuczar.run(.{ .icon = "paperplane" }, appLoop);
 }
